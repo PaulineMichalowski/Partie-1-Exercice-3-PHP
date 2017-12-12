7f904c680d41c398564cb40bf815314cd3b742d0
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-12 11:12:47 +0100

    Création de l'exercice 3

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$km = 1;
+echo $km . ' km à pied, ça use, ça useuh !'  ;
+$km = 3;
+echo ' ' . $km . ' kms à pied, ça use, ça useuh !';
+$km = 25;
+echo ' ' . $km . ' kms à pied, ça use les souliers !';
+?>
